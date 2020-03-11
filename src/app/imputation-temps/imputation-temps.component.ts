29d0570be1292f35b0dd11b42d2d6a9595eb4bc0
kévin SPINICCI <kevin.spinicci@outlook.com>
2020-03-11 14:24:26 +0100

    Ajout component imputation temps

---
+++ src/app/imputation-temps/imputation-temps.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-imputation-temps',
+  templateUrl: './imputation-temps.component.html',
+  styleUrls: ['./imputation-temps.component.css']
+})
+export class ImputationTempsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
