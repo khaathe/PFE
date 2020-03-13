0074856593f5f72e367ad7892abbc963e4ec4f07
kévin SPINICCI <kevin.spinicci@outlook.com>
2020-03-13 12:13:35 +0100

    Avant passage à angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ImputationTempsComponent } from './imputation-temps/imputation-temps.component';
+import {MatInputModule} from '@angular/material/input'; 
+import {MatSelectModule} from '@angular/material/select'; 
+import {MatCheckboxModule} from '@angular/material/checkbox'; 
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { ImputationTempsComponent } from './imputation-temps/imputation-temps.co
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
