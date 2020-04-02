778fa24a57d076c37e1f58ee8ef7338a9211f890
kévin SPINICCI <kevin.spinicci@outlook.com>
2020-04-02 16:56:28 +0200

    Ajout Calendar dans imputation temps + modif height textarea

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ImputationTempsComponent } from './imputation-temps/imputation-temps.component';
+import { CalendarComponent } from './calendar/calendar.component';
 import {MatInputModule} from '@angular/material/input'; 
 import {MatSelectModule} from '@angular/material/select'; 
 import {MatCheckboxModule} from '@angular/material/checkbox'; 
 import {MatButtonModule} from '@angular/material/button';
-import { CalendarComponent } from './calendar/calendar.component';
 
 import { FullCalendarModule } from '@fullcalendar/angular';
 
--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
 
 @Component({
   selector: 'app-calendar',
@@ -14,7 +16,31 @@ export class CalendarComponent implements OnInit {
     
   }
   
-  calendarPlugins = [dayGridPlugin];
+  defaultView = "dayGridMonth";
+  calendarPlugins = [dayGridPlugin, timeGridPlugin, listPlugin];
   locale = "fr";
-  
+  firstDay = 1;
+  aspectRatio = 2.0;
+  header = {
+    left:   'title',
+    center: 'prevYear,prev today next,nextYear',
+    right:  'timeGridWeek,dayGridMonth listWeek'
+  };
+  buttonText = {
+    today:    'Aujourd\'hui',
+    month:    'Mois',
+    week:     'Semaine',
+    day:      'Jour',
+    list:     'Liste Semaine'
+  };
+  events=[ {title : 'Titre', date : '2020-04-02'} ];
+
+  addEvent(){
+
+  }
+
+  removeEvent(){
+    
+  }
+
 }
