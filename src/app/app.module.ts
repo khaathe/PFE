8c9b9842704e2ca0dba0b3c25e11d9f3a7e6c4f5
kévin SPINICCI <kevin.spinicci@outlook.com>
2020-04-09 15:28:41 +0200

    ajout sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { ImputationTempsComponent } from './imputation-temps/imputation-temps.component';
-import { CalendarComponent } from './calendar/calendar.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {ImputationTempsComponent} from './imputation-temps/imputation-temps.component';
+import {CalendarComponent} from './calendar/calendar.component';
 import {MatInputModule} from '@angular/material/input'; 
 import {MatSelectModule} from '@angular/material/select'; 
 import {MatCheckboxModule} from '@angular/material/checkbox'; 
 import {MatButtonModule} from '@angular/material/button';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
 import { FullCalendarModule } from '@fullcalendar/angular';
 
 @NgModule({
@@ -27,7 +27,8 @@ import { FullCalendarModule } from '@fullcalendar/angular';
     MatSelectModule,
     MatCheckboxModule,
     MatButtonModule,
-    FullCalendarModule
+    FullCalendarModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
