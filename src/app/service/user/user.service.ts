c93298a83b003b8730e33b4cde2cd0be37839d0d
kévin SPINICCI <kevin.spinicci@outlook.com>
2020-05-07 08:36:39 +0200

    ajout action user

--- src/app/service/user/user.service.ts
+++ src/app/service/user/user.service.ts
@@ -1,22 +1,40 @@
 import { Injectable } from '@angular/core';
+import { User } from 'src/app/model/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  private user : any;
+  private _user : User;
 
   constructor() {
-    this.user = null;
+    this._user = new User();
+    this._user.name = 'SPINICCI';
+    this._user.firstName = 'Kévin';
+    this._user.role = 'ADMIN';
    }
 
-  setUser = function (user) {
-    this.user = user;
+  set user (user) {
+    this._user = user;
   }
 
-  getUser = function () {
-    return this.user;
+  get user () {
+    return this._user;
+  }
+
+  getUserActions = function() : Array<any>{
+    let actions = [
+      { text : "Imputer le temps", route : "imputations-temps"},
+      { text : "Changer son mot de passe", route : ""},
+      { text : "Exporter les données", route : ""},
+      { text : "Créer une tâche", route : ""},
+      { text : "Clôturer une tâche", route : ""},
+      { text : "Ré-activer une tâche", route : ""},
+      { text : "Ajouter un utilisateur", route : ""},
+      { text : "Supprimer un utilisateur", route : ""}
+    ];
+    return actions;
   }
 
 }
