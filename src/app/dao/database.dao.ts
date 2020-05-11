0a51fc1b1b979cafc83b619f06141e257eae3c1c
kévin SPINICCI <kevin.spinicci@outlook.com>
2020-05-11 09:37:28 +0200

    ajout dao database

---
+++ src/app/dao/database.dao.ts
@@ -0,0 +1,18 @@
+export class DatabaseDAO {
+
+    //TODO: insérer le code pour dialoguer avec la base de données
+
+    public connectUser (idU : string, password : string) {
+
+    }
+
+    public deconnectUser (){
+
+    }
+
+    //pour centraliser la manière dont tu fais tes requêtes
+    public executeQuery (sqlQuery : string) : any{
+
+    }
+
+}
