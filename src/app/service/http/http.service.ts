4aec659c09fab528581cb522b3b79d0bce4d17d0
kévin SPINICCI <kevin.spinicci@outlook.com>
2020-06-16 10:37:14 +0200

    gestion erreur

--- src/app/service/http/http.service.ts
+++ src/app/service/http/http.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http'
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,22 @@ export class HttpService {
   }
 
   public get(url) : Observable<any>{
-    return this.http.get(this.backAdress + url);
+    return this.http.get(this.backAdress + url).pipe( catchError(this.handleError));
+  }
+
+  public handleError (error : any){
+      //TODO: gérer les erreurs à l'aide d'un service
+      if (error.error instanceof ErrorEvent) {
+        // A client-side or network error occurred. Handle it accordingly.
+        console.error('An error occurred:', error.error.message);
+      } else {
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong,
+        console.error(
+          `Backend returned code ${error.status}, ` +
+          `body was: ${error.error}`);
+      }
+      // return an observable with a user-facing error message
+      return throwError('Something bad happened; please try again later.');
   }
 }
