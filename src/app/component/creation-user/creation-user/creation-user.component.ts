45bfadc283db0cd913e69ec880970dba9f1ccf88
kévin SPINICCI <kevin.spinicci@outlook.com>
2020-06-17 16:59:35 +0200

    création user

--- src/app/component/creation-user/creation-user/creation-user.component.ts
+++ src/app/component/creation-user/creation-user/creation-user.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/service/user/user.service';
+
+interface Role {
+  code : string;
+  libelle : string;
+}
 
 @Component({
   selector: 'app-creation-user',
   templateUrl: './creation-user.component.html',
-  styleUrls: ['./creation-user.component.css']
+  styleUrls: ['./creation-user.component.scss']
 })
 export class CreationUserComponent implements OnInit {
 
-  constructor() { }
+  idU : string;
+
+  password : string;
+
+  nom : string;
+
+  prenom : string;
+
+  role : string;
+
+  listRoles : Array<Role>;
+
+  constructor(private userService : UserService) { 
+    this.userService.getRole().subscribe( (response) => this.listRoles=response );
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() : any {
+    this.userService.createUser(this.idU, this.password, this.nom, this.prenom, this.role).subscribe( (response) => console.log(response) );
   }
 
 }
